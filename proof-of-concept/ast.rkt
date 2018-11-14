e0738a53b0acfbf154fc0cfae41ff1cdaba6d371
yuchong-pan <panyuchong@gmail.com>
2018-11-13 20:12:18 -0800

    Rewrote AST in Typed Racket

--- proof-of-concept/ast.rkt
+++ proof-of-concept/ast.rkt
@@ -1,29 +1,43 @@
-#lang plai
+#lang typed/racket
 
-(define-type Pgrm
-  [pgrm (stmts (listof Stmt?))])
+(struct pgrm
+  ([stmts : (Listof Stmt)]))
 
 (define-type Stmt
-  [s-expr (e Expr?)]
-  [s-decl (name symbol?)
-          (type Type?)]
-  [s-assn (vars (listof symbol?))
-          (e Expr?)]
-  [s-func (fname symbol?)
-          (args (listof (cons/c symbol? Type?)))
-          (rets (listof (cons/c symbol? Type?)))
-          (body (listof Stmt?))])
+  (U Expr decl assn func))
 
 (define-type Expr
-  [e-id (name symbol?)]
-  [e-num (n number?)]
-  [e-app (f Expr?) (args (listof Expr?))]
-  [e-binop (op procedure?) (lhs Expr?) (rhs Expr?)])
+  (U Symbol Number Boolean String app binop))
+
+(struct app
+  ([fun  : Expr]
+   [args : (Listof Expr)]))
+
+(struct binop
+  ([op  : Procedure]
+   [lhs : Expr]
+   [rhs : Expr]))
+
+(struct decl
+  ([name : Symbol]
+   [type : Type]))
+
+(struct assn
+  ([vars : (Listof Symbol)]
+   [expr : Expr]))
+
+(struct func
+  ([name : Symbol]
+   [args : (Listof (Pair Symbol Type))]
+   [rets : (Listof (Pair Symbol Type))]
+   [body : (Listof Stmt)]))
 
 (define-type Type
-  [t-int]
-  [t-bool]
-  [t-str]
-  [t-dyn]
-  [t-none]
-  [t-func (dom Type?) (cod Type?)])
+  (U 'int 'boolean 'string 'dynamic 'none arrow star))
+
+(struct arrow
+  ([dom : Type]
+   [cod : Type]))
+
+(struct star
+  ([list : (Listof Type)]))
